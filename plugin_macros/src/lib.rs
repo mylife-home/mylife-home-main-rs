5bc6f460f19f1f56d214d8e5d6c8e0789f85bf91
vincent-tr <vincent@stoic.com>
2022-08-12 19:17:31 +0400

    wip

--- plugin_macros/src/lib.rs
+++ plugin_macros/src/lib.rs
@@ -1,8 +1,53 @@
+use plugin_runtime::metadata::{PluginUsage, ConfigType, Type};
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use darling::{FromDeriveInput, FromField};
 
-#[proc_macro_attribute]
-pub fn plugin(attr: TokenStream, item: TokenStream) -> TokenStream {
-    println!("attr: \"{}\"", attr.to_string());
-    println!("item: \"{}\"", item.to_string());
-    item
+#[derive(FromDeriveInput, Default)]
+#[darling(attributes(plugin), supports(struct_any))]
+struct PluginAttribute {
+    ident: syn::Ident,
+
+    name: Option<String>,
+    decription: Option<String>,
+    usage: PluginUsage,
+}
+
+#[derive(FromField, Default)]
+#[darling(attributes(config))]
+struct ConfigAttribute {
+    ident: Option<syn::Ident>,
+    ty: syn::Type,
+
+    name: Option<String>,
+    decription: Option<String>,
+    type: Option<ConfigType>,
+}
+
+#[derive(FromField, Default)]
+#[darling(attributes(state))]
+struct StateAttribute {
+    ident: Option<syn::Ident>,
+    ty: syn::Type,
+
+    name: Option<String>,
+    decription: Option<String>,
+    value_type: Option<Type>,
+}
+
+#[derive(FromField, Default)]
+#[darling(attributes(action))]
+struct ActionAttribute {
+    ident: Option<syn::Ident>,
+    ty: syn::Type,
+
+    name: Option<String>,
+    decription: Option<String>,
+    value_type: Option<Type>,
+}
+
+#[proc_macro_derive(MylifePlugin, attributes(plugin, config, state, action))]
+pub fn mylife_plugin(input: TokenStream) -> TokenStream {
+    println!("PLUGIN attr: \"{}\"", input.to_string());
+    TokenStream::new()
 }
--- plugin_runtime/src/lib.rs
+++ plugin_runtime/src/lib.rs
@@ -29,3 +29,21 @@ impl<T> Deref for State<T> {
       &self.value
   }
 }
+
+pub struct Config<T> {
+  value: T,
+  // init
+}
+
+impl<T> Deref for Config<T> {
+  type Target = T;
+
+  fn deref(&self) -> &Self::Target {
+      &self.value
+  }
+}
+
+pub struct Action<T> {
+  value: T,
+  // handler
+}
