2b59b2183e961eb20f7110ccaa990a9bab73a58f
vincent-tr <vincent@stoic.com>
2022-08-15 18:35:26 +0400

    wip runtime

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+pub struct PluginRuntime {
+    plugin: Box<dyn Plugin>,
+}
+
+impl PluginRuntime {
+    pub fn configure(&mut self, config: ConfigMap) {
+        // TODO
+    }
+
+    // TODO: state
+    // TODO: action
+
+    pub fn init(&mut self) {
+        self.plugin.init();
+    }
+
+    pub fn terminate(&mut self) {
+        self.plugin.terminate();
+    }
+}
+
+pub struct ConfigMap {}
--- plugin_runtime/src/lib.rs
+++ plugin_runtime/src/lib.rs
@@ -22,35 +22,10 @@ impl PluginData {
         &self.metadata
     }
 
-    pub fn create(&self) -> PluginRuntime {
-        PluginRuntime {
-            plugin: (self.factory)(),
-        }
+    pub fn create(&self) -> Box<dyn Plugin> {
+        (self.factory)()
     }
 }
-
-pub struct PluginRuntime {
-    plugin: Box<dyn Plugin>,
-}
-
-impl PluginRuntime {
-    pub fn configure(&mut self, config: ConfigMap) {
-        // TODO
-    }
-
-    // TODO: state
-    // TODO: action
-
-    pub fn init(&mut self) {
-        self.plugin.init();
-    }
-
-    pub fn terminate(&mut self) {
-        self.plugin.terminate();
-    }
-}
-
-pub struct ConfigMap {}
 
 pub trait StateDef {}
 
