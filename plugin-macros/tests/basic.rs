997f6d1479a291daeaaf1b660c6a191e2f533a47
vincent-tr <vincent@stoic.com>
2023-08-09 22:59:20 +0200

    add test assertions

--- plugin-macros/tests/basic.rs
+++ plugin-macros/tests/basic.rs
@@ -1,5 +1,5 @@
 use plugin_macros::{mylife_actions, MylifePlugin};
-use plugin_runtime::{MylifePluginHooks, State};
+use plugin_runtime::{MylifePluginHooks, State, MylifePlugin, runtime::MylifePluginRuntime, metadata::{PluginUsage, ConfigType, MemberType, Type}};
 
 #[derive(MylifePlugin)]
 #[mylife_plugin(name = "plugin-name", description = "plugin description", usage = "logic")]
@@ -41,9 +41,40 @@ impl Basic {
 
 #[test]
 fn test_basic() {
-    assert_eq!(1, 1);
+  let runtime: Box<dyn MylifePluginRuntime> = Basic::runtime();
+  let meta = runtime.metadata();
+
+  assert_eq!(meta.name(), "plugin-name");
+  assert_eq!(meta.description().unwrap_or("<no desc>"), "plugin description");
+  assert_eq!(meta.usage(), PluginUsage::Logic);
+
+  assert_eq!(meta.config().len(), 1);
+  let (name, config_item) = meta.config().iter().next().unwrap();
+  assert_eq!(name, "configName");
+  assert_eq!(config_item.description().unwrap_or("<no desc>"), "config description");
+  assert_eq!(config_item.value_type(), ConfigType::Bool);
+
+  assert_eq!(meta.members().len(), 2);
+  let mut keys = Vec::from_iter(meta.members().keys());
+  keys.sort();
+
+  let name = keys[0];
+  let member = meta.members().get(name).unwrap();
+  assert_eq!(name, "actionName");
+  assert_eq!(member.description().unwrap_or("<no desc>"), "action description");
+  assert_eq!(member.member_type(), MemberType::Action);
+  assert_eq!(*member.value_type(), Type::Bool);
+
+  let name = keys[1];
+  let member = meta.members().get(name).unwrap();
+  assert_eq!(name, "stateName");
+  assert_eq!(member.description().unwrap_or("<no desc>"), "state description");
+  assert_eq!(member.member_type(), MemberType::State);
+  assert_eq!(*member.value_type(), Type::Bool);
+
 }
 
 // TODO
 // test auto naming
 // test auto type
+// pas de description
