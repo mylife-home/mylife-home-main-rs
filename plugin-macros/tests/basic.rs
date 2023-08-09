2d902a5929af495beac37daf73ddd612a0ebd60d
vincent-tr <vincent@stoic.com>
2023-08-09 23:28:26 +0200

    move metadata tests to utils

--- plugin-macros/tests/basic.rs
+++ plugin-macros/tests/basic.rs
@@ -1,13 +1,29 @@
 use plugin_macros::{mylife_actions, MylifePlugin};
-use plugin_runtime::{MylifePluginHooks, State, MylifePlugin, runtime::MylifePluginRuntime, metadata::{PluginUsage, ConfigType, MemberType, Type}};
+use plugin_runtime::{
+    metadata::{ConfigType, PluginUsage, Type},
+    runtime::MylifePluginRuntime,
+    MylifePlugin, MylifePluginHooks, State,
+};
+
+use crate::utils::TestMetadata;
+
+mod utils;
 
 #[derive(MylifePlugin)]
-#[mylife_plugin(name = "plugin-name", description = "plugin description", usage = "logic")]
+#[mylife_plugin(
+    name = "plugin-name",
+    description = "plugin description",
+    usage = "logic"
+)]
 pub struct Basic {
-    #[mylife_config(name = "configName", description = "config description", r#type="bool")]
+    #[mylife_config(
+        name = "configName",
+        description = "config description",
+        r#type = "bool"
+    )]
     config_value: bool,
 
-    #[mylife_state(name = "stateName", description = "state description", r#type="bool")]
+    #[mylife_state(name = "stateName", description = "state description", r#type = "bool")]
     state_value: State<bool>,
 }
 
@@ -15,8 +31,8 @@ pub struct Basic {
 impl MylifePluginHooks for Basic {
     fn new(_id: &str) -> Self {
         Basic {
-          config_value: Default::default(),
-          state_value: Default::default(),
+            config_value: Default::default(),
+            state_value: Default::default(),
         }
     }
 
@@ -26,14 +42,17 @@ impl MylifePluginHooks for Basic {
 }
 
 impl Drop for Basic {
-  fn drop(&mut self) {
-  }
+    fn drop(&mut self) {}
 }
 
 #[mylife_actions]
 impl Basic {
     // can return Result<(), Box<dyn std::error::Error>> or nothing
-    #[mylife_action(name = "actionName", description = "action description", r#type="bool")]
+    #[mylife_action(
+        name = "actionName",
+        description = "action description",
+        r#type = "bool"
+    )]
     fn action_value(&mut self, _arg: bool) -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     }
@@ -41,37 +60,20 @@ impl Basic {
 
 #[test]
 fn test_basic() {
-  let runtime: Box<dyn MylifePluginRuntime> = Basic::runtime();
-  let meta = runtime.metadata();
+    let runtime: Box<dyn MylifePluginRuntime> = Basic::runtime();
+    let meta = runtime.metadata();
 
-  assert_eq!(meta.name(), "plugin-name");
-  assert_eq!(meta.description().unwrap_or("<no desc>"), "plugin description");
-  assert_eq!(meta.usage(), PluginUsage::Logic);
+    let mut expected = TestMetadata::new(
+        "plugin-name",
+        Some("plugin description"),
+        PluginUsage::Logic,
+    );
 
-  assert_eq!(meta.config().len(), 1);
-  let (name, config_item) = meta.config().iter().next().unwrap();
-  assert_eq!(name, "configName");
-  assert_eq!(config_item.description().unwrap_or("<no desc>"), "config description");
-  assert_eq!(config_item.value_type(), ConfigType::Bool);
+    expected.add_config("configName", Some("config description"), ConfigType::Bool);
+    expected.add_state("stateName", Some("state description"), Type::Bool);
+    expected.add_action("actionName", Some("action description"), Type::Bool);
 
-  assert_eq!(meta.members().len(), 2);
-  let mut keys = Vec::from_iter(meta.members().keys());
-  keys.sort();
-
-  let name = keys[0];
-  let member = meta.members().get(name).unwrap();
-  assert_eq!(name, "actionName");
-  assert_eq!(member.description().unwrap_or("<no desc>"), "action description");
-  assert_eq!(member.member_type(), MemberType::Action);
-  assert_eq!(*member.value_type(), Type::Bool);
-
-  let name = keys[1];
-  let member = meta.members().get(name).unwrap();
-  assert_eq!(name, "stateName");
-  assert_eq!(member.description().unwrap_or("<no desc>"), "state description");
-  assert_eq!(member.member_type(), MemberType::State);
-  assert_eq!(*member.value_type(), Type::Bool);
-
+    assert_eq!(TestMetadata::from_metadata(meta), expected);
 }
 
 // TODO
---
+++ plugin-macros/tests/utils.rs
@@ -0,0 +1,111 @@
+use plugin_runtime::metadata::{ConfigType, MemberType, PluginMetadata, PluginUsage, Type};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct TestMetadata {
+    name: String,
+    description: Option<String>,
+    usage: PluginUsage,
+    config: Vec<TestConfigItem>,
+    members: Vec<TestMember>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct TestConfigItem {
+    name: String,
+    description: Option<String>,
+    value_type: ConfigType,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct TestMember {
+    name: String,
+    description: Option<String>,
+    member_type: MemberType,
+    value_type: Type,
+}
+
+impl TestMetadata {
+    pub fn from_metadata(source: &PluginMetadata) -> Self {
+        let mut meta = TestMetadata::new(source.name(), source.description(), source.usage());
+
+        for (name, config_item) in source.config() {
+            meta.add_config(name, config_item.description(), config_item.value_type());
+        }
+
+        for (name, member) in source.members() {
+            match member.member_type() {
+                MemberType::Action => {
+                    meta.add_action(name, member.description(), member.value_type().clone());
+                }
+                MemberType::State => {
+                    meta.add_state(name, member.description(), member.value_type().clone());
+                }
+            }
+        }
+
+        meta
+    }
+
+    pub fn new(name: &str, description: Option<&str>, usage: PluginUsage) -> Self {
+        TestMetadata {
+            name: String::from(name),
+            description: description.map(str::to_string),
+            usage: usage,
+            config: Vec::new(),
+            members: Vec::new(),
+        }
+    }
+
+    pub fn add_config(
+        &mut self,
+        name: &str,
+        description: Option<&str>,
+        value_type: ConfigType,
+    ) -> &mut Self {
+        self.config.push(TestConfigItem {
+            name: String::from(name),
+            description: description.map(str::to_string),
+            value_type: value_type,
+        });
+
+        self.config.sort_by_key(|item| item.name.clone());
+
+        self
+    }
+
+    pub fn add_state(
+        &mut self,
+        name: &str,
+        description: Option<&str>,
+        value_type: Type,
+    ) -> &mut Self {
+        self.members.push(TestMember {
+            name: String::from(name),
+            description: description.map(str::to_string),
+            member_type: MemberType::State,
+            value_type: value_type,
+        });
+
+        self.members.sort_by_key(|member| member.name.clone());
+
+        self
+    }
+
+    pub fn add_action(
+        &mut self,
+        name: &str,
+        description: Option<&str>,
+        value_type: Type,
+    ) -> &mut Self {
+        self.members.push(TestMember {
+            name: String::from(name),
+            description: description.map(str::to_string),
+            member_type: MemberType::Action,
+            value_type: value_type,
+        });
+
+        self.members.sort_by_key(|member| member.name.clone());
+
+        self
+    }
+}
