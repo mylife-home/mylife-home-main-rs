40645b10924ba77ac20b62fa00ef0f673be4456b
vincent-tr <vincent.tr@gmail.com>
2026-05-27 18:57:50 +0200

    wip registry

--- common/src/components/registry.rs
+++ common/src/components/registry.rs
@@ -1,7 +1,8 @@
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use crate::components::metadata::PluginMetadata;
+use crate::components::{metadata::PluginMetadata, observable::{Observable, ObserverId, Subject}};
 
+/// Component represents a component that can be registered to the registry.
 pub trait Component {
     fn id(&self) -> &str;
 }
@@ -55,21 +56,26 @@ impl InstancePluginKey {
     }
 }
 
+/// Registry is responsible for managing the plugins and components of all instances, and providing an observable interface for other modules to subscribe to registry events.
 pub struct Registry {
     plugins_per_instance: HashMap<InstancePluginKey, Rc<PluginMetadata>>,
     components: HashMap<String, Rc<RefCell<dyn Component>>>,
     instances: HashMap<String, Rc<RefCell<InstanceData>>>,
+    subject: Subject<RegistryEvent>,
 }
 
 impl Registry {
+    /// Creates a new Registry instance.
     pub fn new() -> Self {
         Self {
             plugins_per_instance: HashMap::new(),
             components: HashMap::new(),
             instances: HashMap::new(),
+            subject: Subject::new(),
         }
     }
 
+    /// Registers a plugin to the registry.
     pub fn registry_plugin(&mut self, instance_name: &str, plugin: Rc<PluginMetadata>) {
         let key = InstancePluginKey::new(instance_name, plugin.id());
         if self.plugins_per_instance.contains_key(&key) {
@@ -95,10 +101,39 @@ impl Registry {
             plugin.id(),
             instance_name
         );
-        // TODO: emit event: plugin.add
+        self.subject.notify(&RegistryEvent::PluginAdded {
+            instance_name: instance_name.to_owned(),
+            plugin,
+        });
     }
 
+    /// Unregisters a plugin from the registry.
     pub fn unregistry_plugin(&mut self, instance_name: &str, plugin_id: &str) {
         // TODO
     }
+}
+
+impl Observable<RegistryEvent> for Registry {
+    fn observe(&mut self, observer: impl Fn(&RegistryEvent) + 'static) -> ObserverId {
+        self.subject.observe(observer)
+    }
+
+    fn unobserve(&mut self, id: ObserverId) -> bool {
+        self.subject.unobserve(id)
+    }
+}
+
+/// RegistryEvent represents the events that can occur in the registry, such as adding or removing a plugin or component.
+pub enum RegistryEvent {
+    /// PluginAdded is emitted when a plugin is added to the registry, containing the instance name and the plugin metadata.
+    PluginAdded{ instance_name: String, plugin: Rc<PluginMetadata> },
+
+    /// PluginRemoved is emitted when a plugin is removed from the registry, containing the instance name and the plugin metadata.
+    PluginRemoved{ instance_name: String, plugin: Rc<PluginMetadata> },
+
+    /// ComponentAdded is emitted when a component is added to the registry, containing the instance name and the component.
+    ComponentAdded{ instance_name: String, component: Rc<RefCell<dyn Component>> },
+
+    /// ComponentRemoved is emitted when a component is removed from the registry, containing the instance name and the component.
+    ComponentRemoved{ instance_name: String, component: Rc<RefCell<dyn Component>> },
 }
