cb6e236a72da87c7934b8477be6d0bca93e94839
vincent-tr <vincent.tr@gmail.com>
2026-06-11 21:32:45 +0200

    value_binary test async implementation

--- core/plugins/logic-base/src/value_binary.rs
+++ core/plugins/logic-base/src/value_binary.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt, mem,
+    sync::{Arc, Mutex, MutexGuard},
+};
+
 use log::debug;
 
 use plugin_macros::{MylifePlugin, mylife_actions};
@@ -16,7 +21,7 @@ pub struct ValueBinary {
     #[mylife_state(description = "actual value")] // type=, name=
     state: State<bool>,
 
-    _waker: WakeHandle,
+    updates: Arc<UpdateSync<ValueBinary>>,
 }
 
 // impl Drop if terminate needed
@@ -26,7 +31,7 @@ impl MylifePluginHooks for ValueBinary {
             id: String::from(id),
             config: Default::default(),
             state: Default::default(),
-            _waker: waker,
+            updates: Arc::new(UpdateSync::new(waker)),
         }
     }
 
@@ -37,6 +42,11 @@ impl MylifePluginHooks for ValueBinary {
 
         Ok(())
     }
+
+    fn async_handler(&mut self) {
+        let updates = self.updates.clone();
+        updates.run(self);
+    }
 }
 
 #[mylife_actions]
@@ -45,7 +55,18 @@ impl ValueBinary {
     #[mylife_action(description = "set value to on")] // type=, name=
     fn on(&mut self, arg: bool) -> anyhow::Result<()> {
         if arg {
-            self.state.set(true);
+            //self.state.set(true);
+            // showcase async work
+            tokio::spawn({
+                let updates = self.updates.clone();
+                async move {
+                    updates.enqueue({
+                        move |comp: &mut Self| {
+                            comp.state.set(true);
+                        }
+                    });
+                }
+            });
         }
 
         Ok(())
@@ -62,6 +83,51 @@ impl ValueBinary {
     fn toggle(&mut self, arg: bool) {
         if arg {
             self.state.set(!self.state.get());
+        }
+    }
+}
+
+struct UpdateSync<ComponentType> {
+    queue: Mutex<Vec<Box<dyn FnOnce(&mut ComponentType) + Send>>>,
+    waker: WakeHandle,
+}
+
+impl<ComponentType> fmt::Debug for UpdateSync<ComponentType> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("UpdateSync")
+            .field("queue", &self.queue().len())
+            .field("waker", &self.waker)
+            .finish()
+    }
+}
+
+impl<ComponentType> UpdateSync<ComponentType> {
+    pub fn new(waker: WakeHandle) -> Self {
+        Self {
+            queue: Mutex::new(Vec::new()),
+            waker,
+        }
+    }
+
+    fn queue(&self) -> MutexGuard<'_, Vec<Box<dyn FnOnce(&mut ComponentType) + Send>>> {
+        self.queue.lock().expect("cannot lock mutex")
+    }
+
+    pub fn enqueue<Update: FnOnce(&mut ComponentType) + Send + 'static>(&self, update: Update) {
+        self.queue().push(Box::new(update));
+        self.waker.wake();
+    }
+
+    pub fn run(&self, component: &mut ComponentType) {
+        let queue = {
+            let mut storage = Vec::new();
+            let mut current = self.queue();
+            mem::swap(&mut storage, &mut current);
+            storage
+        };
+
+        for update in queue {
+            update(component);
         }
     }
 }
