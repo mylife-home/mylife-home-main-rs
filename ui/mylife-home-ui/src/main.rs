85e88653b05f1f950852098fbbf200ef81045eb7
vincent-tr <vincent.tr@gmail.com>
2026-07-07 10:57:15 +0200

    add build tooling

--- ui/mylife-home-ui/src/main.rs
+++ ui/mylife-home-ui/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use common::{
+    ActorsConfig, instance_info,
+    utils::{actors::SpawnedActors, config, logger, wait_for_shutdown_signal},
+};
+
+#[derive(Parser, Debug)]
+#[command(name = "mylife-home-ui")]
+#[command(about = "Mylife Home UI")]
+struct Cli {
+    /// config file
+    #[arg(long, default_value = "config.toml")]
+    config: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    config::init(&cli.config);
+    logger::init();
+
+    let mut actors = SpawnedActors::new().await;
+
+    common::init(
+        &mut actors,
+        "ui",
+        &ActorsConfig {
+            listen_remote_metadata: true,
+            listen_remote_logs: false,
+        },
+    )
+    .await;
+
+    // store::init_actor(&mut actors).await;
+
+    let instance_info_handle = instance_info::InstanceInfoPublisherHandle::new();
+    instance_info_handle.add_component("ui", env!("CARGO_PKG_VERSION"));
+
+    wait_for_shutdown_signal().await;
+
+    actors.terminate().await;
 }
