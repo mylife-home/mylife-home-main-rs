51833505d7f0c313f90cdaef6bfde56f5cc331b9
vincent-tr <vincent.tr@gmail.com>
2026-07-23 17:43:55 +0200

    fix output folder

--- studio/web-app/bundler/vite.config.ts
+++ studio/web-app/bundler/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(base, 'dist'),
     emptyOutDir: true,
     // rollupOptions: {
     //   // Seems side effects are dropped else
