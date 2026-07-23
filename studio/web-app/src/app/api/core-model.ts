f36bc8fe2b4c6fcbe2b0f72a6d5d8dc27c3571e3
vincent-tr <vincent.tr@gmail.com>
2026-07-23 18:10:13 +0200

    add missing defs

--- studio/web-app/src/app/api/core-model.ts
+++ studio/web-app/src/app/api/core-model.ts
@@ -1,3 +1,27 @@
 // direct import to avoid require all common in core
 // we depend on mylife-home-core only for this
-export * from 'mylife-home-core/dist/store/model';
+// export * from 'mylife-home-core/dist/store/model';
+
+export const enum StoreItemType {
+  COMPONENT = 'component',
+  BINDING = 'binding'
+}
+
+export interface StoreItem {
+  readonly type: StoreItemType;
+  readonly config: ComponentConfig | BindingConfig;
+}
+
+export interface ComponentConfig {
+  readonly id: string;
+  readonly plugin: string;
+  readonly config: { [name: string]: any };
+}
+
+export interface BindingConfig {
+  readonly sourceComponent: string;
+  readonly sourceState: string;
+  readonly targetComponent: string;
+  readonly targetAction: string;
+}
+
--- studio/web-app/src/app/api/online.ts
+++ studio/web-app/src/app/api/online.ts
@@ -1,8 +1,43 @@
 import { Component, Plugin } from './component-model';
 // direct import to avoid require all common in ui
-import { InstanceInfo } from 'mylife-home-common/dist/instance-info/types';
+// import { InstanceInfo } from 'mylife-home-common/dist/instance-info/types';
 
-export { InstanceInfo };
+// export { InstanceInfo };
+
+export interface InstanceInfo {
+  /**
+   * 'ui' | 'studio' | 'core' | 'driver? (for arduino/esp/...)'
+   */
+  type: string;
+
+  /**
+   * main: Raspberry ... | nodemcu | x64
+   * others are details like ram, cpu, ...
+   */
+  hardware: { [name: string]: string };
+  /**
+   * --- rpi
+   * os: linux-xxx
+   * node: 24.5
+   * mylife-home-core: 1.0.0
+   * mylife-home-common: 1.0.0
+   * --- esp/arduino
+   * mylife: 1.21.4
+   */
+  versions: {
+    [component: string]: string;
+  };
+
+  systemUptime: number;
+  instanceUptime: number;
+  hostname: string;
+  capabilities: string[];
+
+  wifi?: {
+    rssi: number;
+  }
+}
+
 
 export interface UpdateInstanceInfoData {
   operation: 'set' | 'clear';
