3a6dac112d110dd1e6912d9af80df09a08a100c1
vincent-tr <vincent.tr@gmail.com>
2026-09-10 16:01:08 +0200

    Add paths

--- studio/mylife-home-studio/src/services/mod.rs
+++ studio/mylife-home-studio/src/services/mod.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+use common::utils::config;
 use common::{InitData, utils::actors::SpawnedActors};
 
 use crate::web::DispatcherBuilder;
@@ -7,11 +11,71 @@ mod logging;
 mod online;
 mod project_manager;
 
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GitConfig {
+    pub app_url: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct PathsConfig {
+    pub root: PathBuf,
+    pub project_manager: ProjectManagerPaths,
+    pub deploy: DeployPaths,
+}
+
+impl PathsConfig {
+    pub fn resolve(&mut self) {
+        self.root = Self::resolve_path(
+            &env::current_dir().expect("failed to get current directory"),
+            &self.root,
+        );
+
+        self.project_manager.core = Self::resolve_path(&self.root, &self.project_manager.core);
+        self.project_manager.ui = Self::resolve_path(&self.root, &self.project_manager.ui);
+
+        self.deploy.files = Self::resolve_path(&self.root, &self.deploy.files);
+        self.deploy.recipes = Self::resolve_path(&self.root, &self.deploy.recipes);
+        self.deploy.pinned_recipes_file =
+            Self::resolve_path(&self.root, &self.deploy.pinned_recipes_file);
+    }
+
+    fn resolve_path(base: &Path, child: &Path) -> PathBuf {
+        if child.is_absolute() {
+            child.to_path_buf()
+        } else {
+            base.join(child)
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ProjectManagerPaths {
+    pub ui: PathBuf,
+    pub core: PathBuf,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct DeployPaths {
+    pub files: PathBuf,
+    pub recipes: PathBuf,
+    pub pinned_recipes_file: PathBuf,
+}
+
 pub async fn init(
     actors: &mut SpawnedActors,
     dispatcher: &mut DispatcherBuilder,
     init_data: &InitData,
 ) {
+    let git: GitConfig = config::section("git");
+    let mut paths: PathsConfig = config::section("paths");
+    paths.resolve();
+
     online::init(actors, dispatcher, init_data).await;
     git::init(actors, dispatcher).await;
     logging::init(actors, dispatcher).await;
